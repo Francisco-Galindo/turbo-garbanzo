0371ee696457e04955e63641f2bcf1273863282a
Francisco-Galindo <paqui10718@gmail.com>
2021-06-07 16:37:22 -0500

    DOCS: Comentando el inicio de sesion

--- dynamics/php/inicio_sesion.php
+++ dynamics/php/inicio_sesion.php
@@ -4,14 +4,16 @@ require 'config.php';
 require 'seguridad_y_cripto.php';
 
 $_POST['num_cuenta'] = '320054336';
-$_POST['contrasena'] = 'popo';
+$_POST['contrasena'] = 'popol';
 
 $conexion = conectar_base();
-// $_POST = purgar_arreglo($_POST, $conexion);
+// Purgando el arreglo $_POST de posibles ataques
+$_POST = purgar_arreglo($_POST, $conexion);
 $error = array(false);
 
+// Asignando los valores recibidos a variables
 $num_cuenta = floatval($_POST['num_cuenta']) === (float)0 ?
-        null : floatval($_POST['num_cuenta']);
+	null : floatval($_POST['num_cuenta']);
 
 $contrasena = isset($_POST['contrasena']) && $_POST['contrasena'] != '' ?
 	$_POST['contrasena'] : null;
@@ -19,49 +21,50 @@ $contrasena = isset($_POST['contrasena']) && $_POST['contrasena'] != '' ?
 // Validando los datos recibidos
 if ($contrasena === null) {
 	$error[0] = true;
-	array_push($error, 'Contraseña no válida');
+	array_push($error, 'Contraseña no inexistente');
 }
 if ($num_cuenta === null || strlen($num_cuenta) !== 9) {
 	$error[0] = true;
 	array_push($error, 'Número de cuenta no válido');
 }
 
-echo $num_cuenta . '<br>';
-echo $contrasena . '<br>';
 
-if (!$error[0]) {
-        $num_cuenta_hash = hash('sha256', $num_cuenta);
-        $contrasena_hash = hash('sha256', $contrasena);
+// Comparando la contraseña con la de la base de datos
+if ($error[0] === false) {
+	$num_cuenta_hash = hash('sha256', $num_cuenta);
+	$contrasena_hash = hash('sha256', $contrasena);
 
-        $consulta = "SELECT contrasena, sal FROM usuario 
-                WHERE id_usuario='$num_cuenta_hash';";
-        $resultado = mysqli_query($conexion, $consulta);
+	$consulta = "SELECT contrasena, sal FROM usuario
+		WHERE id_usuario='$num_cuenta_hash';";
+	$resultado = mysqli_query($conexion, $consulta);
 
-        if(mysqli_num_rows($resultado) === 0) {
-                $error[0] = true;
-	        array_push($error, 'No existe tal usuario');
-        } else {
-                $row = mysqli_fetch_array($resultado);
-                $hash_bd = $row['contrasena'];
-                $sal = $row['sal'];
+	if(mysqli_num_rows($resultado) === 0) {
+		$error[0] = true;
+		array_push($error, 'No existe tal usuario');
+	} else {
+		$row = mysqli_fetch_array($resultado);
+		$hash_bd = $row['contrasena'];
+		$sal = $row['sal'];
 
-                $correcto = verificar_contrasena_sha256(
-                        $contrasena,
-                        $sal,
-                        $hash_bd
-                );
-                if (!$correcto) {
-                        $error[0] = true;
-	                array_push($error, 'Contraseña incorrecta'); 
-                }
-        }
+		$correcto = verificar_contrasena_sha256(
+			$contrasena,
+			$sal,
+			$hash_bd
+		);
+		if (!$correcto) {
+			$error[0] = true;
+			array_push($error, 'Contraseña incorrecta');
+		}
+	}
 }
 
+// Enviando la respuesta
 if ($error[0] === false) {
 	echo 'Exito';
 } else {
-	for ($i = 1; $i < count($error); $i++) {
-		echo $error[$i];
+	array_shift($error);
+	foreach ($error as $valor) {
+		echo $valor;
 		echo '|';
 	}
 }
