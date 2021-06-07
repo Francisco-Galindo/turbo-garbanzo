11eb40821d4abc0992d1227a7cd9e9318cf7b0a0
Niunk <gerechtigkeit@comunidad.unam.mx>
2021-06-06 21:08:35 -0500

    FEAT: crear nueva asesoría php

--- dynamics/php/busqueda.php
+++ dynamics/php/busqueda.php
@@ -7,17 +7,17 @@ $busqueda = (isset($_POST['busqueda']) && $_POST['busqueda'] !== "") ? $_POST['b
 if ($busqueda !== "No hay búsqueda") {
     $consulta = 'SELECT (nombre, materia, tema, fecha_hora, duracion_simple, medio_vir, lugar) FROM usuario NATURAL JOIN asesoria_has_usuario NATURAL JOIN asesoria NATURAL JOIN materia WHERE nombre=' . $busqueda . ' OR materia=' . $busqueda;
     $res = mysqli_query($conexion, $consulta);
-}
-while ($row = mysqli_fetch_array($res)) {
-    echo 'Nombre: ' . $row[1] . ' Materia: ' . $row[2] . '<br>';
-    echo 'Tema: ' . $row[3] . '<br>';
-    $fecha = date("d-m-Y h:i A", $row[4]);
-    echo 'Fecha: ' . $fecha . '<br>';
-    $duracion = ($row[6] === true) ? "50 min" : "100 min";
-    echo 'Duración: ' . $duracion . '        ';
-    $medio = ($row[7] === true) ? "Virtual" : "Presencial";
-    echo 'Medio: ' . $medio . '<br>';
-    echo 'Lugar: ' . $row[8];
+    while ($row = mysqli_fetch_array($res)) {
+        echo 'Nombre: ' . $row[1] . ' Materia: ' . $row[2] . '<br>';
+        echo 'Tema: ' . $row[3] . '<br>';
+        $fecha = date("d-m-Y h:i A", $row[4]);
+        echo 'Fecha: ' . $fecha . '<br>';
+        $duracion = ($row[6] === true) ? "50 min" : "100 min";
+        echo 'Duración: ' . $duracion . '        ';
+        $medio = ($row[7] === true) ? "Virtual" : "Presencial";
+        echo 'Medio: ' . $medio . '<br>';
+        echo 'Lugar: ' . $row[8];
+    }
 }
 
 // EOF
--- dynamics/php/crear_asesoria.php
+++ dynamics/php/crear_asesoria.php
@@ -4,12 +4,19 @@
         $conexion = conectar_base();
         $usuario = $_SESSION['usuario'];
 
-        $fecha = $_POST['fecha'];
-        $hora = $_POST['hora'];
-        $duracion = $_POST['duracion'];
+        $fecha = explode("-",$_POST['fecha']);
+        $hora = explode(":", $_POST['hora']);
+        $fechaHora = mktime(intval($hora[0]), intval($hora[1]), 0, intval($fecha[1]), intval($fecha[2]), intval($fecha[0]));
+        $duracion = (($_POST['duracion'])=="true")?true:false;
         $tema = $_POST['tema'];
         $materia = $_POST['materia'];
-        $cupo = $_POST[''];
-        $datos_as = 'INSERT INTO asesoria (id_usuario,id_materia,tema,fecha_hora,duracion_simple,cupo,medio_vir,lugar) VALUES (';
+        $cupo = intval($_POST['cupo']);
+        $medio = (($_POST['medio'])=="true")?true:false;
+        $lugar = $_POST['lugar'];
+
+        $datos_as = 'INSERT INTO asesoria (id_usuario,id_materia,tema,fecha_hora,duracion_simple,cupo,medio_vir,lugar) VALUES ('.$usuario.','.$materia.',"'.$tema.'",'.$fechaHora.','.$duracion.','.$cupo.','.$medio.',"'.$lugar.'")';
+        $ins = mysqli_query($conexion, $datos_as);
+        ($ins)?echo 'Registro de asesoría exitoso':echo 'Registro fallido';
     } 
-?>
+
+// EOF
