812fe624778dacda7b1e734d9818668973c08e37
Francisco-Galindo <paqui10718@gmail.com>
2021-06-07 09:44:42 -0500

    A

--- dynamics/php/prueba.php
+++ dynamics/php/prueba.php
@@ -1,42 +1,22 @@
-<?php
-
-define("PASSWORD", "popo");
-define("HASH", "sha256");
-define("METHOD", "aes-128-cbc");
-
-echo "<h1>Cifrado>:|</h1>";
-
-$key = openssl_digest(PASSWORD, HASH);
-// echo $key . "<br>";
-$iv_len = openssl_cipher_iv_length(METHOD);
-$iv = openssl_random_pseudo_bytes($iv_len);
-// echo $iv_len . "<br>";
-// echo $iv . "<br>";
-
-$cifrado = openssl_encrypt(
-        "curso web 2021:)",
-        METHOD,
-        'popo',
-        OPENSSL_RAW_DATA,
-        $iv
-);
-$cifrado = base64_encode($cifrado);
-echo 'cifrado: ' . $cifrado . "<br>";
-// //!cast5.cbc
-$cifrado = 'c0l3LaVbKTjO1MuFZZq3BhWxACSp+zEc6t+J4JPOQHk=';
-echo 'cifrado loleado: ' . $cifrado . "<br>";
-$decifrado = openssl_decrypt(
-        $cifrado,
-        METHOD,
-        'popo',
-        OPENSSL_RAW_DATA,
-        $iv
-);
-echo $decifrado;
-echo '<br>';
-// $pass = "supremacyKiki1_";
-// $hasheo = password_hash($pass, PASSWORD_BCRYPT);
-// echo $hasheo;
-// echo '<br>';
-// echo strlen($hasheo);
-// echo '<br>';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+        <h1>Hola</h1>
+        <script>
+                function suma(n, m) {
+                        return n + m;
+                }
+                console.log(suma(4, 5));
+                document.querySelector('h1').innerHTML = suma(4, 5);
+        </script>
+        <?php
+        echo "XXD"
+        ?>
+</body>
+</html>
