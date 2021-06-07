f0e0b19e6169541c0aee7bfa94088276f6eb896b
Francisco-Galindo <paqui10718@gmail.com>
2021-06-06 21:58:23 -0500

    S

--- dynamics/php/registro.php
+++ dynamics/php/registro.php
@@ -6,7 +6,7 @@ require 'cripto.php';
 $_POST['num_cuenta'] = '320054336';
 $_POST['contrasena'] = 'popo';
 $_POST['correo'] = 'paqui10718@gmail.com';
-$_POST['grado'] = 'cuarto';
+$_POST['grado'] = 'sexto';
 $_POST['telefono'] = '5522442070';
 $_POST['nombre'] = 'Francisco';
 $_POST['prim_ape'] = 'Galindo';
@@ -46,61 +46,69 @@ $fecha_nacimiento = isset($_POST['fecha_nacimiento']) && $_POST['fecha_nacimient
 
 
 if ($contrasena === null) {
+	$error[0] = true;
 	array_push($error, 'Contraseña no válida');
 }
 if ($num_cuenta === null || strlen($num_cuenta) !== 9) {
+	$error[0] = true;
 	array_push($error, 'Número de cuenta no válido');
 }
 
 $regex = '/^[\w\.\-\ñ]{4,20}(\.([\w\.\-]))*@([\w\.\-]+)(\.[\w\.\-]+)/';
 if (!preg_match($regex, $correo)) {
+	$error[0] = true;
 	array_push($error, 'Correo no válido');
 }
 
 if (!in_array($_POST['grado'], array('cuarto', 'quinto', 'sexto'))) {
+	$error[0] = true;
 	array_push($error, 'Grado no válido');
 }
 
 if ($_POST['telefono'] === null|| strlen($_POST['telefono']) !== 10 ) {
+	$error[0] = true;
 	array_push($error, 'Teléfono no válido');
 }
 
 if (time() - strtotime($_POST['fecha_nacimiento']) < 0) {
+	$error[0] = true;
 	array_push($error, 'Fecha no válida');
 }
 
-foreach ($error as $cadena) {
+if ($error[0] === false) {
+	$pimienta = obtener_pimienta();
+	$sal = obtener_sal();
+	$hash = hash('sha256', $contrasena . $pimienta . $sal);
+	
+	
+	$num_cuenta_cifrado = cifrar_cadena($num_cuenta, $contrasena);
+	$telefono_cifrado = cifrar_cadena($telefono, $contrasena);
+	
+	
+	$consulta = "INSERT INTO usuario 
+		(contrasena, sal, num_cuenta, correo, grado, telefono, nombre, prim_ape, seg_ape, fecha_nacimiento) VALUES ('$hash', '$sal', '$num_cuenta_cifrado', '$correo', 
+		'$grado', '$telefono_cifrado', '$nombre', '$prim_ape', '$seg_ape', '$fecha_nacimiento');";
+	
+	$resultado = mysqli_query($conexion, $consulta);
+	if ($resultado === false) {
+		echo 'Muerte';
+		echo '<br>';
+	}
+	
+	$consulta = "SELECT * FROM usuario WHERE correo='$correo';";
+	$resultado = mysqli_query($conexion, $consulta);
+	$row = mysqli_fetch_array($resultado);
+	
+	echo 'WTF2: ' . descifrar_cadena($row['num_cuenta'], $contrasena) . '<br>';
+	echo 'WTF3: ' . descifrar_cadena($row['telefono'], $contrasena) . '<br>';
+	
+}
+
+mysqli_close($conexion);
+
+foreach ($error as $indx => $cadena) {
 	echo $cadena;
 	echo '<br>';
 }
-
-
-$pimienta = obtener_pimienta();
-$sal = obtener_sal();
-$hash = hash('sha256', $contrasena . $pimienta . $sal);
-
-
-$num_cuenta_cifrado = cifrar_cadena($num_cuenta, $contrasena);
-$telefono_cifrado = cifrar_cadena($telefono, $contrasena);
-
-
-$consulta = "INSERT INTO usuario 
-	(contrasena, sal, num_cuenta, correo, grado, telefono, nombre, prim_ape, seg_ape, fecha_nacimiento) VALUES ('$hash', '$sal', '$num_cuenta_cifrado', '$correo', 
-	'$grado', '$telefono_cifrado', '$nombre', '$prim_ape', '$seg_ape', '$fecha_nacimiento');";
-
-$resultado = mysqli_query($conexion, $consulta);
-if ($resultado === false) {
-	echo 'Muerte';
-	echo '<br>';
-}
-
-$consulta = "SELECT * FROM usuario WHERE correo='$correo';";
-$resultado = mysqli_query($conexion, $consulta);
-$row = mysqli_fetch_array($resultado);
-
-echo 'WTF2: ' . descifrar_cadena($row['num_cuenta'], $contrasena) . '<br>';
-echo 'WTF3: ' . descifrar_cadena($row['telefono'], $contrasena) . '<br>';
-
-mysqli_close($conexion);
 
 // EOF
