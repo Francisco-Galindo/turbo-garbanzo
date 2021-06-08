5de0b924a5b6729f30363563d6edef1802394bfe
Niunk <gerechtigkeit@comunidad.unam.mx>
2021-06-08 00:17:36 -0500

    FEAT: vista de asesoría

--- dynamics/php/busqueda.php
+++ dynamics/php/busqueda.php
@@ -3,7 +3,22 @@ require_once 'config.php';
 
 $conexion = conectar_base();
 date_default_timezone_set("America/Mexico_City");
+$filFech = (isset($_POST['fecha']))?$_POST['fecha']:null;
+$filMat = (isset($_POST['materia']))?$_POST['fecha']:null;
+$filMod = (isset($_POST['modalidad'])?$_POST['modalidad']:null;
 $busqueda = (isset($_POST['busqueda']) && $_POST['busqueda'] !== "") ? $_POST['busqueda'] : "No hay búsqueda";
+
+
+
+if(isset($filFech) && !(isset($filMat)) && !(isset($filMod))){
+	$cons = "SELECT (nombre, materia, tema, fecha_hora, duracion_simple, medio_vir, lugar) FROM usuario WHERE fecha_hora LIKE '$filFech'%";		
+}else if(!(isset($filFech)) && (isset($filMat)) && !(isset($filMod))){
+	$cons = "SELECT (nombre, materia, tema, fecha_hora, duracion_simple, medio_vir, lugar) FROM usuario WHERE materia LIKE '$filMat'";
+}else if(!(isset($filFech)) && !(isset($filMat)) && (isset($filMod))){
+	
+}
+$resFil = mysqli_query($conexion, $cons);
+
 if ($busqueda !== "No hay búsqueda") {
 	$consulta = 'SELECT (nombre, materia, tema, fecha_hora, duracion_simple, medio_vir, lugar) FROM usuario NATURAL JOIN asesoria_has_usuario NATURAL JOIN asesoria NATURAL JOIN materia WHERE nombre=' . $busqueda . ' OR materia=' . $busqueda;
 	$res = mysqli_query($conexion, $consulta);
