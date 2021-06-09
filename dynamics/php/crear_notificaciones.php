0494f762543871ee3f0126993f1637915fcd42d6
Francisco-Galindo <paqui10718@gmail.com>
2021-06-09 13:56:57 -0500

    FEAT: Creando notificaciones de strikes y confirmaciones

--- dynamics/php/crear_notificaciones.php
+++ dynamics/php/crear_notificaciones.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * Iterar por las asesorias que da el usuario
- * 	Si la asesoria ya terminó / emepezo y no existe notificacion al respecto,
- * 	crear la notificacion
- * 
- * Iterar por las asesorias en las que esta inscrito
- * 	Mismo proceso que arriba, pero las notificaciones faltan 
- */
-
-require 'config.php';
-require 'seguridad_y_cripto.php';
-
 function notificar_inicio_asesoria(
 	$conexion, 
 	$id_asesoria, 
@@ -64,6 +52,38 @@ function notificar_final_asesoria(
 
 }
 
+function notificar_strike(
+	$conexion,
+	$id_usuario,
+	$id_asesoria
+	)
+{
+	$consulta = "SELECT id_tipo_notificacion FROM tipo_notificacion
+		WHERE titulo='aviso_strike';";
+	$resultado = mysqli_query($conexion, $consulta);
+	$row = mysqli_fetch_assoc($resultado);
+	$id_aviso = $row['id_tipo_notificacion'];
+
+	notificar_sobre_sesion($conexion, $id_asesoria, $id_usuario, 
+		[$id_aviso]);
+}
+
+function notificar_solicitud_asesoria_asesor(
+	$conexion,
+	$id_usuario,
+	$id_asesoria
+)
+{
+	$consulta = "SELECT id_tipo_notificacion FROM tipo_notificacion
+		WHERE titulo='confirmar_asesoria';";
+	$resultado = mysqli_query($conexion, $consulta);
+	$row = mysqli_fetch_assoc($resultado);
+	$id_aviso = $row['id_tipo_notificacion'];
+
+	notificar_sobre_sesion($conexion, $id_asesoria, $id_usuario, 
+	[$id_aviso]);
+}
+
 function notificar_sobre_sesion(
 	$conexion,
 	$id_asesoria,
@@ -95,47 +115,4 @@ function notificar_sobre_sesion(
 	}
 }
 
-$conexion = conectar_base();
-session_start();
-if (isset($_SESSION)) {
-	$_SESSION = purgar_arreglo($_SESSION, $conexion);
-}
-
-$id_usuario = $_SESSION['id_usuario'];
-
-
-
-$consulta = "SELECT id_asesoria, fecha_hora FROM asesoria 
-	WHERE id_usuario='$id_usuario'";
-$resultado = mysqli_query($conexion, $consulta);
-
-while ($row = mysqli_fetch_assoc($resultado)) {
-	$id_asesoria = $row['id_asesoria'];
-	if (strtotime($row['fecha_hora']) - time() <= 0 ) {
-		notificar_inicio_asesoria($conexion, 
-			$id_asesoria, 
-			$id_usuario,
-			true);
-	}
-}
-
-$consulta = "SELECT id_asesoria FROM asesoria_has_usuario
-	WHERE id_usuario='$id_usuario';";
-$resultado = mysqli_query($conexion, $consulta);
-
-while ($row = mysqli_fetch_assoc($resultado)) {
-	$id_asesoria = $row['id_asesoria'];
-	$consulta = "SELECT id_asesoria, fecha_hora FROM asesoria 
-		WHERE id_asesoria='$id_asesoria'";
-	$resultado = mysqli_query($conexion, $consulta);
-
-	while ($row = mysqli_fetch_assoc($resultado)) {
-		if (strtotime($row['fecha_hora']) - time() <= 0 ) {
-			notificar_inicio_asesoria($conexion, 
-				$id_asesoria, 
-				$id_usuario,
-				false);
-		}
-	}
-}
- //EOF
+// EOF
