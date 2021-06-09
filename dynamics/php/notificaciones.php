a403e6ec2aebbc7bb4d7a9acf1384cb8035c5708
Francisco-Galindo <paqui10718@gmail.com>
2021-06-09 12:30:45 -0500

    FEAT: Creando notificaciones al inicio y final de las mismmas

--- dynamics/php/notificaciones.php
+++ dynamics/php/notificaciones.php
@@ -12,23 +12,130 @@
 require 'config.php';
 require 'seguridad_y_cripto.php';
 
+function notificar_inicio_asesoria(
+	$conexion, 
+	$id_asesoria, 
+	$id_usuario,
+	$es_asesor
+)
+{
+	$tipos_notificacion = array();
+	if ($es_asesor) {
+		$consulta = "SELECT id_tipo_notificacion FROM tipo_notificacion
+			WHERE titulo='aviso_inicio'
+			OR titulo='pasar_asistencia';";
+	} else {
+		$consulta = "SELECT id_tipo_notificacion FROM tipo_notificacion
+			WHERE titulo='aviso_inicio';";
+	}
+	$resultado = mysqli_query($conexion, $consulta);
+	while ($row = mysqli_fetch_assoc($resultado)) {
+		array_push($tipos_notificacion, $row['id_tipo_notificacion']);
+	}
+
+	notificar_sobre_sesion($conexion, $id_asesoria, $id_usuario, 			$tipos_notificacion);
+
+}
+
+function notificar_final_asesoria(
+	$conexion,
+	$id_asesoria, 
+	$id_usuario,
+	$es_asesor
+)
+{
+	$tipos_notificacion = array();
+	if ($es_asesor) {
+		$consulta = "SELECT id_tipo_notificacion FROM tipo_notificacion
+			WHERE titulo='aviso_final'
+			OR titulo='reportar';";
+	} else {
+		$consulta = "SELECT id_tipo_notificacion FROM tipo_notificacion
+			WHERE titulo='aviso_final'
+			OR titulo='reportar'
+			OR titulo='valorar';";
+	}
+	$resultado = mysqli_query($conexion, $consulta);
+	while ($row = mysqli_fetch_assoc($resultado)) {
+		array_push($tipos_notificacion, $row['id_tipo_notificacion']);
+	}
+
+	notificar_sobre_sesion($conexion, $id_asesoria, $id_usuario, 			$tipos_notificacion);
+
+}
+
+function notificar_sobre_sesion(
+	$conexion,
+	$id_asesoria,
+	$id_usuario,
+	$tipos_notificacion
+)
+{
+	$cadena_ids = '(';
+	foreach ($tipos_notificacion as $id_tipo) {
+		$cadena_ids .= 'id_tipo_notificacion=' . $id_tipo . ' OR ';
+	}
+	$cadena_ids .= '0=1)';
+	$consulta = "SELECT id_tipo_notificacion FROM notificacion
+		WHERE id_usuario='$id_usuario'
+		AND $cadena_ids";
+	$resultado = mysqli_query($conexion, $consulta);
+	echo $consulta;
+	// $row = mysqli_fetch_assoc($resultado);
+	if (mysqli_num_rows($resultado) === 0) {
+		foreach ($tipos_notificacion as $id_tipo) {
+			$consulta = "INSERT INTO notificacion 
+				(id_usuario, id_asesoria, 
+				id_tipo_notificacion)
+				VALUES ('$id_usuario', $id_asesoria, 
+				$id_tipo);";
+			$resultado = mysqli_query($conexion, $consulta);
+		}
+
+	}
+}
+
 $conexion = conectar_base();
 session_start();
 if (isset($_SESSION)) {
 	$_SESSION = purgar_arreglo($_SESSION, $conexion);
 }
+
 $id_usuario = $_SESSION['id_usuario'];
+
+
 
 $consulta = "SELECT id_asesoria, fecha_hora FROM asesoria 
 	WHERE id_usuario='$id_usuario'";
 $resultado = mysqli_query($conexion, $consulta);
 
 while ($row = mysqli_fetch_assoc($resultado)) {
-	echo $row['id_asesoria'] . '<br>';
-	echo $row['fecha_hora'] . '<br>';
-	echo strtotime($row['fecha_hora']) - time() . '<br>';
+	$id_asesoria = $row['id_asesoria'];
 	if (strtotime($row['fecha_hora']) - time() <= 0 ) {
-		
+		notificar_inicio_asesoria($conexion, 
+			$id_asesoria, 
+			$id_usuario,
+			true);
+	}
+}
+
+$consulta = "SELECT id_asesoria FROM asesoria_has_usuario
+	WHERE id_usuario='$id_usuario';";
+$resultado = mysqli_query($conexion, $consulta);
+
+while ($row = mysqli_fetch_assoc($resultado)) {
+	$id_asesoria = $row['id_asesoria'];
+	$consulta = "SELECT id_asesoria, fecha_hora FROM asesoria 
+		WHERE id_asesoria='$id_asesoria'";
+	$resultado = mysqli_query($conexion, $consulta);
+
+	while ($row = mysqli_fetch_assoc($resultado)) {
+		if (strtotime($row['fecha_hora']) - time() <= 0 ) {
+			notificar_inicio_asesoria($conexion, 
+				$id_asesoria, 
+				$id_usuario,
+				false);
+		}
 	}
 }
  //EOF
