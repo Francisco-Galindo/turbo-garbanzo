5ff1000a35a406bbd6b171ef4ed8aaa82f4ebda7
jaz-ramirez <jazmin.ramirez@comunidad.unam.mx>
2021-06-09 16:12:53 -0500

    FEAT: primeras peticiones y  regex

--- dynamics/js/crearCuenta.js
+++ dynamics/js/crearCuenta.js
@@ -13,6 +13,14 @@ $(document).ready(function() {
         let arch = $("#arch").val();
         let password = $("#password").val();
 
+        let regexCorreo= /^[\w\.\-\ñ]{4,20}(\.([\w\.\-]))*@([\w\.\-]+)(\.[\w\.\-]+)/;
+        let reguexCuenta = /^[1-3]\d{8}/;
+        let reguexNames = /[A-Za-zñÑá-úÁ-Ú]{2,32}/;
+        let reguexTel = /^[1-9]\d{9}$/;
+        let reguexImg = /^[A-Za-zá-úÁ-Ú0-9_\-()\/&%$#!¡¿?]{1,50}\.(jpg|png|jpeg)$/;
+        let reguexContrasena = /[\wñÑ_\-\/\.&%$#!?¿¡]{6,20}/; 
+
+
         let peticion= $.ajax({
             url: "../dynamics/php/registro.php",
             data: {num_cuenta:noCuenta, 
