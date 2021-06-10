32eac6278a1f0ca3e6d4be0e2acc79159a2da98f
Niunk <gerechtigkeit@comunidad.unam.mx>
2021-06-09 22:58:40 -0500

    FEAT: primera version de validaciones de crear asesoría por js y php

--- dynamics/php/crear_asesoria.php
+++ dynamics/php/crear_asesoria.php
@@ -1,16 +1,47 @@
 <?php
+$arrOpCorr = array();
+function validaciones($numPet){
+	if($numPet == 0){
+		
+		$arrOpCorr[0] = true;
+	}else if($numPet == 1){
+		
+		$arrOpCorr[1] = true;
+	}else if($numPet == 2){
+		$arrOpCorr[2] = true;
+	}else if($numPet == 3){
+		$arrOpCorr[3] = true;
+	}else if($numPet == 4){
+		$arrOpCorr[4] = true;
+	}else if($numPet == 5){
+		$arrOpCorr[5] = true;
+	}else if($numPet == 6){
+		$arrOpCorr[6] = true;
+	}else if($numPet == 7){
+		$arrOpCorr[7] = true;
+	}else if($numPet == 8){
+		foreach($arrOpCorr as $key => $value){
+			$arrOpCorr[8] = ($value===true && $arrOpCorr[8]===true)?true:false;
+		}
+	}
+	return $res;
+}
+
 $_SESSION['usuario'] = '9ecea6b0b95158e3336fb8701242281706ec48692be23a8c2eb523798eaddf07';
-$_POST['fecha'] = "2021-05-05";
+/*$_POST['fecha'] = "2021-05-05";
 $_POST['hora'] = "19:05";
 $_POST['duracion'] = "true";
 $_POST['tema'] = "Funciones cuadráticas";
 $_POST['materia'] = "1500";
 $_POST['cupo'] = "2";
 $_POST['medio'] = "true";
-$_POST['lugar'] = "Escuela Nacional Preparatoria Plantel 6 Antonio Caso";
+$_POST['lugar'] = "Escuela Nacional Preparatoria Plantel 6 Antonio Caso";*/
 
+$peticion = $_POST['pet'];
 
-if (isset($_SESSION['usuario'])) {
+$valor = validaciones($peticion);
+
+if (isset($_SESSION['usuario']) && $arrOpCorr[8]===true) {
 	include('config.php');
 	$conexion = conectar_base();
 	$usuario = $_SESSION['usuario'];
