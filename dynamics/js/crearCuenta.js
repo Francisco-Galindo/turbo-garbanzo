6f909a06cc6f541b7055cb13769e5ad41db04f97
jaz-ramirez <jazmin.ramirez@comunidad.unam.mx>
2021-06-09 20:23:02 -0500

    FEAT: Validaciones para inicio de sesión y crear asesoria

--- dynamics/js/crearCuenta.js
+++ dynamics/js/crearCuenta.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
         let reguexImg = /^[A-Za-zá-úÁ-Ú0-9_\-()\/&%$#!¡¿?]{1,50}\.(jpg|png|jpeg)$/;
         let reguexContrasena = /[\wñÑ_\-\/\.&%$#!?¿¡]{6,20}/; 
 
-
+        
         let peticion= $.ajax({
             url: "../dynamics/php/registro.php",
             data: {num_cuenta:noCuenta, 
